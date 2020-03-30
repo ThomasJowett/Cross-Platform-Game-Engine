b28f6ff66e3a61db70e0b4d0217f3051dcf2e832
ThomasJowett <thomas_jowett@hotmail.co.uk>
2020-03-30 23:48:35 +0100

    Added some example logger messages to the example game, added some hint on how to use the logging system

--- ExampleGame/src/ExampleApp.cpp
+++ ExampleGame/src/ExampleApp.cpp
@@ -6,6 +6,7 @@
 ExampleGame::ExampleGame(const WindowProps& props)
 	:Application(props)
 {
+	// Sets the clear colour of the screen, by default is black
 	RenderCommand::SetClearColour(Colours::GREY);
 
 	//PushLayer(new GridLayer());
@@ -16,6 +17,14 @@ ExampleGame::ExampleGame(const WindowProps& props)
 	Application::GetWindow().SetIcon("resources/Circle.png");
 
 	Application::GetWindow().SetWindowMode(WindowMode::WINDOWED, 1920, 1080);
+
+	// Example usage of the logger
+	CLIENT_CRITICAL("[Example Critical] The game is about to crash!");
+	CLIENT_ERROR("[Example Error] Something went wrong!");
+	CLIENT_WARN("[Example Warning] Something may have gone wrong...");
+	CLIENT_INFO("[Example Information] Normal stuff happened");
+	CLIENT_DEBUG("[Example Debug] Diagnostic information");
+	CLIENT_TRACE("[Example Trace] Very fine detailed diagnostic information");
 }
 
 void ExampleGame::OnUpdate()
