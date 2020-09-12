d0cf12ce8f67bbadcb270416917e884fa92e76a9
ThomasJowett <thomas_jowett@hotmail.co.uk>
2020-09-12 17:22:27 +0100

    Updated Submodules

---
+++ UpdateSubmodules.bat
@@ -0,0 +1,19 @@
+cd Engine\vendor\spdlog
+git pull origin v1.x
+
+cd ..\GLFW
+git pull origin master
+
+cd ..\imgui
+git pull origin docking
+
+cd ..\math
+git pull origin master
+
+cd ..\..\..\Editor\vendor\IconFont
+git pull origin master
+
+cd ..\OpenFBX
+git pull origin master
+
+pause
