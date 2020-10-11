90267c16db986fa5858971755432bbfaa74adec4
ThomasJowett <thomas_jowett@hotmail.co.uk>
2020-10-11 23:47:25 +0100

    Removed cpp.hint. Added new line to Components. Fixed template<> in ScriptableEntity lookup table

--- Engine/src/Scene/ScriptableEntity.cpp
+++ Engine/src/Scene/ScriptableEntity.cpp
@@ -1,4 +1,4 @@
 #include "stdafx.h"
 #include "ScriptableEntity.h"
 
-Factory<ScriptableEntity>::map_Type* Factory<ScriptableEntity>::s_LookUpTable = NULL;
+template<>Factory<ScriptableEntity>::map_Type* Factory<ScriptableEntity>::s_LookUpTable = NULL;
