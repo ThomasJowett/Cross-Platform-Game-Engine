1913c924f7173b3bd38a187ae0d2c7a73334c502
ThomasJowett <thomas_jowett@hotmail.co.uk>
2021-07-25 14:18:35 +0100

    Updated submodules and the update submodules batch file, updated stb_image

--- UpdateSubmodules.bat
+++ UpdateSubmodules.bat
@@ -1,22 +1,3 @@
-cd Engine\vendor\spdlog
-git pull origin v1.x
-
-cd ..\GLFW
-git pull origin master
-
-cd ..\imgui
-git pull origin docking
-
-cd ..\math
-git pull origin master
-
-cd ..\cereal
-git pull origin master
-
-cd ..\..\..\Editor\vendor\IconFont
-git pull origin master
-
-cd ..\OpenFBX
-git pull origin master
+git submodule foreach --recursive 'git pull'
 
 pause
