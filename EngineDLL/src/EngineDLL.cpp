63dcf27629ac8ceb95233bc6c89eaeeda366c1af
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2022-02-24 09:24:25 +0000

    Added frame buffer to engine DLL and renders scene

--- EngineDLL/src/EngineDLL.cpp
+++ EngineDLL/src/EngineDLL.cpp
@@ -5,5 +5,10 @@
 ENGINEDLL_API int fnEngineDLL(void)
 {
     Scene* scene = new Scene(std::string("Test"));
+
+    FrameBufferSpecification fbSpec = { 600, 600 };
+    fbSpec.attachments = { FrameBufferTextureFormat::RGBA8, FrameBufferTextureFormat::RED_INTEGER, FrameBufferTextureFormat::Depth };
+    Ref<FrameBuffer> renderTarget = FrameBuffer::Create(fbSpec);
+    scene->Render(renderTarget);
     return (int)scene->GetSceneName().size();
 }
