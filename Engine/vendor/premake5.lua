296795fda17b1693b20d35fc21c5375f15c202fc
ThomasJowett <thomas_jowett@hotmail.co.uk>
2022-03-20 14:54:09 +0000

    added SPIRV-Cross as a submodule

--- Engine/vendor/premake5.lua
+++ Engine/vendor/premake5.lua
@@ -245,3 +245,57 @@ project "LiquidFun"
 		defines "DIST"
 		runtime "Release"
 		optimize "On"
+project "SPIRV-Cross"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++11"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "%{prj.name}")
+
+	files
+	{
+		"SPIRV-Cross/spirv.h",
+		"SPIRV-Cross/spirv.hpp",
+		"SPIRV-Cross/spirv_cfg.hpp",
+		"SPIRV-Cross/spirv_cfg.cpp",
+		"SPIRV-Cross/spirv_common.hpp",
+		"SPIRV-Cross/spirv_cpp.hpp",
+		"SPIRV-Cross/spirv_cpp.cpp",
+		"SPIRV-Cross/spirv_cross.hpp",
+		"SPIRV-Cross/spirv_cross.cpp",
+		"SPIRV-Cross/spirv_cross_c.hpp",
+		"SPIRV-Cross/spirv_cross_c.cpp",
+		"SPIRV-Cross/spirv_cross_containers.hpp",
+		"SPIRV-Cross/spirv_cross_error_handling.hpp",
+		"SPIRV-Cross/spirv_cross_parsed_ir.hpp",
+		"SPIRV-Cross/spirv_cross_parsed_ir.cpp",
+		"SPIRV-Cross/spirv_cross_util.hpp",
+		"SPIRV-Cross/spirv_cross_util.cpp",
+		"SPIRV-Cross/spirv_glsl.cpp",
+		"SPIRV-Cross/spirv_glsl.hpp",
+		"SPIRV-Cross/spirv_hlsl.hpp",
+		"SPIRV-Cross/spirv_hlsl.cpp",
+		"SPIRV-Cross/spirv_msl.hpp",
+		"SPIRV-Cross/spirv_msl.cpp",
+		"SPIRV-Cross/spirv_parser.hpp",
+		"SPIRV-Cross/spirv_parser.cpp",
+		"SPIRV-Cross/spirv_reflect.hpp",
+		"SPIRV-Cross/spirv_reflect.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "On"
+
+	filter "configurations:Distribution"
+		defines "DIST"
+		runtime "Release"
+		optimize "On"
