307cdd90d878d43da916dfa3dfd668b48b688b74
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2023-08-12 10:52:53 +0100

    removed scripts and vendor folders

--- Scripts/Progen_VS2019.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0%/..
-call vendor\bin\premake\premake5.exe vs2019
-IF %ERRORLEVEL% NEQ 0 (PAUSE)
--- Scripts/Progen_Xcode.sh
+++
@@ -1,6 +0,0 @@
-CURRENT_DIR=$(basename "$PWD")
-
-if [[ "$CURRENT_DIR" == "Scripts" ]]; then
-    cd ..
-fi
-./vendor/bin/premake/premake5 xcode4
