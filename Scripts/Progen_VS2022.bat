7d44da3fae0bd3f2dc6878f6932fa86d31c9801b
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2023-08-12 10:52:53 +0100

    removed scripts and vendor folders

--- Scripts/Progen_VS2022.bat
+++
@@ -1,3 +0,0 @@
-cd %~dp0%/..
-call vendor\bin\premake\premake5.exe vs2022
-IF %ERRORLEVEL% NEQ 0 (PAUSE)
--- Scripts/Progen_Xcode_arm.sh
+++
@@ -1,6 +0,0 @@
-CURRENT_DIR=$(basename "$PWD")
-
-if [[ "$CURRENT_DIR" == "Scripts" ]]; then
-    cd ..
-fi
-./vendor/bin/premake/premake5_arm xcode4 -arch="arm64"
