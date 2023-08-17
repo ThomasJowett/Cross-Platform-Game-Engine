f10205c991bee9c6d64084f49285c0e43ba3ad2c
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2023-08-17 14:37:10 +0100

    Enable the appropriate extensions for various platforms

--- Engine/src/Platform/Vulkan/VulkanContext.cpp
+++ Engine/src/Platform/Vulkan/VulkanContext.cpp
@@ -42,6 +42,25 @@ void VulkanContext::Init()
 		VK_KHR_SURFACE_EXTENSION_NAME
 	};
 
+#if defined(_WIN32)
+	instanceExtensions.push_back(VK_KHR_WIN32_SURFACE_EXTENSION_NAME);
+#elif defined(VK_USE_PLATFORM_ANDROID_KHR)
+	instanceExtensions.push_back(VK_KHR_ANDROID_SURFACE_EXTENSION_NAME);
+#elif defined(_DIRECT2DISPLAY)
+	instanceExtensions.push_back(VK_KHR_DISPLAY_EXTENSION_NAME);
+#elif defined(VK_USE_PLATFORM_WAYLAND_KHR)
+	instanceExtensions.push_back(VK_KHR_WAYLAND_SURFACE_EXTENSION_NAME);
+#elif defined(VK_USE_PLATFORM_XCB_KHR)
+	instanceExtensions.push_back(VK_KHR_XCB_SURFACE_EXTENSION_NAME);
+#elif defined(VK_USE_PLATFORM_MACOS_MVK)
+	instanceExtensions.push_back(VK_MVK_MACOS_SURFACE_EXTENSION_NAME);
+#endif
+
+#if defined(VK_USE_PLATFORM_MACOS_MVK) && (VK_HEADER_VERSION >= 216)
+    instanceExtensions.push_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+    instanceExtensions.push_back(VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME);
+#endif
+
 	VkValidationFeatureEnableEXT enables[] = { VK_VALIDATION_FEATURE_ENABLE_BEST_PRACTICES_EXT };
 	VkValidationFeaturesEXT features = {};
 	features.sType = VK_STRUCTURE_TYPE_VALIDATION_FEATURES_EXT;
@@ -54,11 +73,15 @@ void VulkanContext::Init()
 	instanceCreateInfo.pApplicationInfo = &appInfo;
 	instanceCreateInfo.enabledExtensionCount = (uint32_t)instanceExtensions.size();
 	instanceCreateInfo.ppEnabledExtensionNames = instanceExtensions.data();
+#if defined(VK_USE_PLATFORM_MACOS_MVK) && (VK_HEADER_VERSION >= 216)
+    instanceCreateInfo.flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+#endif
 
 	err = vkCreateInstance(&instanceCreateInfo, nullptr, &g_VkInstance);
 
 	if (err != VK_SUCCESS) {
 		ENGINE_CRITICAL("Could not create vulkan instance!");
+		return;
 	}
 
 	volkLoadInstance(g_VkInstance);
