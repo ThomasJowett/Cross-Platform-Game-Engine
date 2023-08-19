25ea811efc738a408f30709c47c62d0ac5fba02e
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2023-08-19 20:11:33 +0100

    Print a string for the vulkan error instead of just the error code

--- Engine/src/Platform/Vulkan/Vulkan.h
+++ Engine/src/Platform/Vulkan/Vulkan.h
@@ -3,11 +3,13 @@
 #define VK_NO_PROTOTYPES
 #include <volk.h>
 
+#include <vulkan/vk_enum_string_helper.h >
+
 #define VK_CHECK_RESULT(f)\
 {\
 	VkResult result = (f);\
 	if (result != VK_SUCCESS)\
 	{\
-		ENGINE_ERROR("VkResult is '{0}' in {1}:{2}", result, __FILE__, __LINE__);\
+		ENGINE_ERROR("VkResult is '{0}' in {1}:{2}", string_VkResult(result), __FILE__, __LINE__); \
 	}\
 }
