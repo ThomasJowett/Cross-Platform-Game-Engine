715558139b80494a219a51e78c8d2abe0fca991b
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2024-12-23 00:26:10 +0000

    implemented most of the WebGPU context class

--- Engine/src/Platform/WebGPU/WebGPUContext.cpp
+++ Engine/src/Platform/WebGPU/WebGPUContext.cpp
@@ -3,12 +3,10 @@
 #include "Logging/Instrumentor.h"
 #include <glad/glad.h>
 
-#pragma comment(lib, "WebGPU32.lib")
+#include <glfw3webgpu.h>
 
-int s_Version;
-
-WebGPUContext::WebGPUContext(GLFWwindow * windowHandle)
-	:m_windowHandle(windowHandle)
+WebGPUContext::WebGPUContext(GLFWwindow* windowHandle)
+	:m_WindowHandle(windowHandle)
 {
 	CORE_ASSERT(windowHandle, "Window Handle is null")
 }
@@ -16,37 +14,92 @@ WebGPUContext::WebGPUContext(GLFWwindow * windowHandle)
 void WebGPUContext::Init()
 {
 	PROFILE_FUNCTION();
-	glfwMakeContextCurrent(m_windowHandle);
+	glfwMakeContextCurrent(m_WindowHandle);
 
-	int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-	CORE_ASSERT(status, "Failed to initialize GLAD");
+	m_Instance = wgpuCreateInstance(nullptr);
+	m_Surface = glfwCreateWindowWGPUSurface(m_Instance, m_WindowHandle);
 
-    const char* version = (const char*)glGetString(GL_VERSION);
-    ENGINE_INFO("Graphics Card: {0} {1}", (const char*)glGetString(GL_VENDOR), (const char*)glGetString(GL_RENDERER));
-	ENGINE_INFO("WebGPU Version: {0}", version);
-    
-    int major, minor;
-    sscanf(version, "%d.%d", &major, &minor);
-    s_Version = major * 100 + minor * 10;
+	wgpu::RequestAdapterOptions adapterOpts{};
+	adapterOpts.compatibleSurface = m_Surface;
+
+	m_Adapter = m_Instance.requestAdapter(adapterOpts);
+
+	wgpu::AdapterProperties properties = {};
+	m_Adapter.getProperties(&properties);
+
+	ENGINE_INFO("Graphics Card: {0}", properties.name);
+
+	wgpu::DeviceDescriptor deviceDesc = {};
+	deviceDesc.label = "Main Device";
+	deviceDesc.requiredFeatureCount = 0;
+	deviceDesc.nextInChain = nullptr;
+	deviceDesc.defaultQueue.label = "The Default queue";
+	deviceDesc.deviceLostCallback = [](WGPUDeviceLostReason reason, char const* message, void* /*pUserData*/)
+	{
+		ENGINE_CRITICAL("Device lost: Reason: {0} Message: {1}", (int)reason, message);
+	};
+
+	m_Device = m_Adapter.requestDevice(deviceDesc);
+
+	auto onDeviceError = [](wgpu::ErrorType type, char const* message)
+	{
+		ENGINE_ERROR("Uncaptured device error: type {0}", (int)type);
+		if (message)
+			ENGINE_ERROR(message);
+	};
+
+	m_ErrorCallbackHandle = m_Device.setUncapturedErrorCallback(std::move(onDeviceError));
+	m_Queue = m_Device.getQueue();
+
+	int width, height;
+	glfwGetWindowSize(m_WindowHandle, &width, &height);
+	m_SurfaceConfig.width = width;
+	m_SurfaceConfig.height = height;
+	m_SurfaceConfig.usage = wgpu::TextureUsage::RenderAttachment;
+	wgpu::TextureFormat surfaceFormat = m_Surface.getPreferredFormat(m_Adapter);
+	m_SurfaceConfig.format = surfaceFormat;
+
+	m_SurfaceConfig.viewFormatCount = 0;
+	m_SurfaceConfig.viewFormats = nullptr;
+	m_SurfaceConfig.device = m_Device;
+	m_SurfaceConfig.presentMode = wgpu::PresentMode::Fifo;
+	m_SurfaceConfig.alphaMode = wgpu::CompositeAlphaMode::Auto;
+
+	m_Surface.configure(m_SurfaceConfig);
 }
 
 void WebGPUContext::SwapBuffers()
 {
 	PROFILE_FUNCTION();
-	glfwSwapBuffers(m_windowHandle);
+	m_Surface.present();
+#if defined(WEBGPU_BACKEND_DAWN)
+	m_Device.tick();
+#elif defined(WEBGPU_BACKEND_WGPU)
+	m_Device.poll(false);
+#endif
 }
 
-void WebGPUContext::SetSwapInterval(uint32_t interval) 
+void WebGPUContext::SetSwapInterval(uint32_t interval)
 {
-	glfwSwapInterval((int)interval);
-}
-
-int WebGPUContext::GetVersion()
-{
-    return s_Version;
+	m_SurfaceConfig.presentMode = interval == 1 ? wgpu::PresentMode::Fifo : wgpu::PresentMode::Immediate;
+	m_Surface.configure(m_SurfaceConfig);
 }
 
 void WebGPUContext::MakeCurrent()
 {
-	glfwMakeContextCurrent(m_windowHandle);
+}
+
+wgpu::Device WebGPUContext::GetWebGPUDevice()
+{
+	return m_Device;
+}
+
+wgpu::TextureFormat WebGPUContext::GetSwapchainFormat()
+{
+	return m_Surface.getPreferredFormat(m_Adapter);
+}
+
+wgpu::Queue WebGPUContext::GetQueue()
+{
+	return m_Queue;
 }
--- Engine/src/Platform/WebGPU/WebGPUContext.h
+++ Engine/src/Platform/WebGPU/WebGPUContext.h
@@ -6,6 +6,8 @@
 
 #include "GLFW/glfw3.h"
 
+#include <webgpu/webgpu.hpp>
+
 class WebGPUContext : public GraphicsContext
 {
 public:
@@ -15,8 +17,20 @@ public:
 	virtual void ResizeBuffers(uint32_t width, uint32_t height) override {};
 	virtual void SetSwapInterval(uint32_t interval) override;
 	virtual void MakeCurrent() override;
+
+	wgpu::Device GetWebGPUDevice();
+	wgpu::TextureFormat GetSwapchainFormat();
+	wgpu::Queue GetQueue();
     
-    static int GetVersion();
 private:
-	GLFWwindow* m_windowHandle;
+	GLFWwindow* m_WindowHandle;
+	wgpu::Instance m_Instance;
+	wgpu::Surface m_Surface;
+	wgpu::Adapter m_Adapter;
+	wgpu::Device m_Device;
+	wgpu::Queue m_Queue;
+
+	wgpu::SurfaceConfiguration m_SurfaceConfig;
+
+	std::unique_ptr<wgpu::ErrorCallback> m_ErrorCallbackHandle;
 };
