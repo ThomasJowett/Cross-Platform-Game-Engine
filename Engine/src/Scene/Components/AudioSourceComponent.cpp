80725945427537000fb3a3c58ecbc53b90fb2e53
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2025-03-12 17:09:31 +0000

    Add audio source component

---
+++ Engine/src/Scene/Components/AudioSourceComponent.cpp
@@ -0,0 +1,21 @@
+#include "cereal/cereal.hpp"
+#include "Assets/Audio.h"
+#include "Utilities/SerializationUtils.h"
+
+struct AudioSourceComponent
+{
+	Ref<Audio> audio;
+private:
+	friend cereal::access;
+	template<typename Archive>
+	void save(Archive& archive) const
+	{
+		SerializationUtils::SaveAssetToArchive(archive, audio);
+	}
+
+	template<typename Archive>
+	void load(Archive& archive)
+	{
+		SerializationUtils::LoadAssetFromArchive(archive, audio);
+	}
+}
