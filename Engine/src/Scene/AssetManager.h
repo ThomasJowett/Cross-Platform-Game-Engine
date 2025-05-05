58a7cd613be4d505de6903d2045a5ed3e7f55577
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2025-05-05 23:20:45 +0100

    add a virtual file system to the asset manager and a method to load an asset bundle

--- Engine/src/Scene/AssetManager.h
+++ Engine/src/Scene/AssetManager.h
@@ -6,22 +6,31 @@
 #include "Logging/Instrumentor.h"
 #include "Asset/Texture.h"
 #include "Core/Factory.h"
+#include "Core/VirtualFileSystem.h"
 
 class AssetManager
 {
 public:
+	static void LoadBundle(const void* zipData, size_t zipSize)
+	{
+		PROFILE_FUNCTION();
+		s_Instance->m_VFS = CreateRef<VirtualFileSystem>();
+		s_Instance->m_VFS->Mount(zipData, zipSize);
+	}
 	template<typename T>
 	static Ref<T> GetAsset(const std::filesystem::path& filepath)
 	{
 		PROFILE_FUNCTION();
 		Ref<Asset> asset = nullptr;
-		asset = AssetManager::Get().m_Assets.Load<T>(filepath);
+		asset = AssetManager::Get().m_Assets.Load<T>(filepath, AssetManager::Get().m_VFS);
 		return std::dynamic_pointer_cast<T>(asset);
 	}
 
 	static Ref<Texture2D> GetTexture(const std::filesystem::path& filepath)
 	{
 		PROFILE_FUNCTION();
+		if(AssetManager::Get().m_VFS)
+			return AssetManager::Get().m_Textures.Load(filepath, AssetManager::Get().m_VFS);
 		return AssetManager::Get().m_Textures.Load(filepath);
 	}
 
@@ -34,6 +43,7 @@ public:
 	{
 		AssetManager::Get().m_Assets.Clear();
 		AssetManager::Get().m_Textures.Clear();
+		AssetManager::Get().m_VFS->Unmount();
 	}
 
 private:
@@ -43,6 +53,7 @@ private:
 
 	AssetLibrary m_Assets;
 	TextureLibrary2D m_Textures;
+	Ref<VirtualFileSystem> m_VFS;
 
 	static AssetManager* s_Instance;
 };
