a630527d2687d06cc6f0545254ce00c2da93cd15
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2025-05-10 18:17:42 +0100

    Added textures to load from memory

--- Engine/src/Asset/Texture.h
+++ Engine/src/Asset/Texture.h
@@ -58,8 +58,6 @@ public:
 
 	virtual void Bind(uint32_t slot = 0) const = 0;
 
-	virtual std::string GetName() const = 0;
-
 	virtual uint32_t GetRendererID() const = 0;
 
 	virtual void Reload() = 0;
@@ -80,20 +78,22 @@ class Texture2D :public Texture
 {
 public:
 	virtual bool Load(const std::filesystem::path& filepath) override;
+	virtual bool Load(const std::filesystem::path& filepath, const std::vector<uint8_t>& data) override;
 	static Ref<Texture2D> Create(uint32_t width, uint32_t height, Format format = Format::RGBA, const void* pixels = nullptr);
 	static Ref<Texture2D> Create(const std::filesystem::path& filepath);
+	static Ref<Texture2D> Create(const std::filesystem::path& filepath, const std::vector<uint8_t>& imageData);
 };
 
 class TextureLibrary2D
 {
 public:
 	void Add(const Ref<Texture2D>& texture);
-	Ref<Texture2D> Load(const std::filesystem::path& path);
-	Ref<Texture2D> Get(const std::string& name);
+	Ref<Texture2D> Load(const std::filesystem::path& path, Ref<VirtualFileSystem> vfs);
+	Ref<Texture2D> Get(const std::filesystem::path& name);
 	void Clear();
 	size_t Size() const { return m_Textures.size(); }
-	bool Exists(const std::string& name) const;
+	bool Exists(const std::filesystem::path& name) const;
 private:
-	std::unordered_map<std::string, Ref<Texture2D>> m_Textures;
+	std::unordered_map<std::filesystem::path, Ref<Texture2D>> m_Textures;
 };
 
