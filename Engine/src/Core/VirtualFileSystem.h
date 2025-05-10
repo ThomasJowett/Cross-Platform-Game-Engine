c50aeeac7fe071feec7c88cf8974cc4b8d541ebc
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2025-05-10 18:29:36 +0100

    change all paths to use std::filesystem::path in virtual file system

--- Engine/src/Core/VirtualFileSystem.h
+++ Engine/src/Core/VirtualFileSystem.h
@@ -1,15 +1,18 @@
 #pragma once
 #include <miniz.h>
+#include <filesystem>
 
 class VirtualFileSystem {
 public:
+	VirtualFileSystem();
+	~VirtualFileSystem();
 	void Mount(const void* zipData, size_t zipSize);
 	void Unmount();
 
-	bool Exists(const std::string& path);
-	bool ReadFile(const std::string& path, std::vector<uint8_t>& data);
+	bool Exists(const std::filesystem::path& path);
+	bool ReadFile(const std::filesystem::path& path, std::vector<uint8_t>& data);
 
 private:
 	mz_zip_archive m_Archive;
-	std::unordered_map<std::string, mz_uint> m_FileIndex;
+	std::unordered_map<std::filesystem::path, mz_uint> m_FileIndex;
 };
