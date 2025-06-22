f341618b41df13ea87eb2b4e26a5219620d6fea9
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2025-06-22 16:18:09 +0100

    add get depth attachment and blit functions to frame buffer class

--- Engine/src/Platform/DirectX/DirectX11FrameBuffer.h
+++ Engine/src/Platform/DirectX/DirectX11FrameBuffer.h
@@ -18,6 +18,10 @@ public:
     virtual int ReadPixel(uint32_t attachmentIndex, int x, int y) override;
 
     virtual Ref<Texture> GetColourAttachment(size_t index) override;
+	virtual Ref<Texture> GetDepthAttachment() override;
+
+	virtual void BlitDepthTo(Ref<FrameBuffer> target) override;
+	virtual void BlitColourTo(Ref<FrameBuffer> target, uint32_t srcAttachmentIndex, uint32_t dstAttachmentIndex) override;
 
     virtual const FrameBufferSpecification& GetSpecification() const override { return m_Specification;  }
 
