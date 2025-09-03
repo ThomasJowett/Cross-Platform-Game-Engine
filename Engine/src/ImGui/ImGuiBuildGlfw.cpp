73ecc72b5d651878ad436a9b4215207216e28eef
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2025-09-03 15:36:52 +0100

    specify that imgui impl is using wgpu

--- Engine/src/ImGui/ImGuiBuildGlfw.cpp
+++ Engine/src/ImGui/ImGuiBuildGlfw.cpp
@@ -2,6 +2,8 @@
 
 #pragma warning(disable:4996)
 
+#define IMGUI_IMPL_WEBGPU_BACKEND_WGPU
+
 #include <imgui/backends/imgui_impl_glfw.cpp>
 #include <imgui/backends/imgui_impl_opengl3.cpp>
 #include <imgui/backends/imgui_impl_wgpu.cpp>
