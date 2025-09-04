6db9b7725dcaefbca2c4349900cbf0fe805e4799
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2025-09-04 23:09:03 +0100

    implement the frame buffer class as best as I can for now

--- Engine/src/Platform/WebGPU/WebGPUFrameBuffer.h
+++ Engine/src/Platform/WebGPU/WebGPUFrameBuffer.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "Renderer/FrameBuffer.h"
+#include "WebGPUTexture.h"
+
+#include <webgpu/webgpu.hpp>
 class WebGPUFrameBuffer :
 	public FrameBuffer
 {
@@ -16,18 +19,23 @@ public:
 
 	virtual int ReadPixel(uint32_t attachmentIndex, int x, int y) override;
 
-	virtual uint32_t GetColourAttachment(size_t index) override;
+	virtual Ref<Texture> GetColourAttachment(size_t index) override;
+	virtual Ref<Texture> GetDepthAttachment() override;
+
+	virtual void BlitDepthTo(Ref<FrameBuffer> target) override;
+	virtual void BlitColourTo(Ref<FrameBuffer> target, uint32_t srcAttachmentIndex, uint32_t dstAttachmentIndex) override;
 
 	virtual const FrameBufferSpecification& GetSpecification() const override { return m_Specification; }
 
 	virtual void ClearAttachment(size_t index, int value) override;
 private:
-
-	uint32_t m_RendererID;
-	std::vector<uint32_t> m_ColourAttachments;
-	uint32_t m_DepthAttachment;
-
 	FrameBufferSpecification m_Specification;
-	std::vector<FrameBufferTextureSpecification> m_ColourAttachmentSpecifications;
-	FrameBufferTextureSpecification m_DepthAttachmentSpecification;
+
+	std::vector<Ref<WebGPUTexture2D>> m_ColourAttachments;
+	Ref<WebGPUTexture2D> m_DepthAttachment;
+
+	std::vector<wgpu::TextureView> m_ColourViews;
+	wgpu::TextureView m_DepthView;
+
+	wgpu::Device m_Device;
 };
