7a5a92fbff87172d07e80274d48ff300a4e59e64
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2026-05-17 18:08:31 +0100

    comment out the start render pass in WebGPU rendererAPI so that it does not cause a crash

--- Engine/src/Platform/WebGPU/WebGPURendererAPI.cpp
+++ Engine/src/Platform/WebGPU/WebGPURendererAPI.cpp
@@ -1,6 +1,6 @@
 #include "WebGPURendererAPI.h"
-#include "Logging/Instrumentor.h"
 #include "Core/Application.h"
+#include "Logging/Instrumentor.h"
 
 bool WebGPURendererAPI::Init()
 {
@@ -14,31 +14,27 @@ bool WebGPURendererAPI::Init()
 	return true;
 }
 
-void WebGPURendererAPI::SetClearColour(const Colour& colour)
-{
-	m_ClearColour = colour;
-}
+void WebGPURendererAPI::SetClearColour(const Colour& colour) { m_ClearColour = colour; }
 
 void WebGPURendererAPI::SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
 {
-	//TODO: recreate the surface
-	//glViewport(x, y, width, height);
+	if (m_RenderPass)
+	{
+		m_RenderPass.setViewport(static_cast<float>(x), static_cast<float>(y), static_cast<float>(width), static_cast<float>(height), 0.0f, 1.0f);
+	}
 }
 
-void WebGPURendererAPI::Clear()
-{
-}
+void WebGPURendererAPI::Clear() {}
 
-void WebGPURendererAPI::ClearColour()
-{
-}
+void WebGPURendererAPI::ClearColour() {}
 
-void WebGPURendererAPI::ClearDepth()
-{
-}
+void WebGPURendererAPI::ClearDepth() {}
 
 void WebGPURendererAPI::StartRenderPass()
 {
+	// TODO: Implement this function
+	// TODO: Pass the target view to this function and get the command encoder
+	/*
 	wgpu::RenderPassDescriptor renderPassDesc = {};
 
 	auto device = m_WebGPUContext->GetWebGPUDevice();
@@ -51,13 +47,17 @@ void WebGPURendererAPI::StartRenderPass()
 	//TODO: render pass descriptor
 
 	m_RenderPass = encoder.beginRenderPass(renderPassDesc);
-
+	*/
 }
 
 void WebGPURendererAPI::EndRenderPass()
 {
-	m_RenderPass.end();
-	m_RenderPass.release();
+	if (m_RenderPass)
+	{
+		m_RenderPass.end();
+		m_RenderPass.release();
+		m_RenderPass = nullptr;
+	}
 }
 
 void WebGPURendererAPI::DrawIndexed(uint32_t indexCount, uint32_t indexStart, uint32_t vertexOffset, bool backFaceCull, DrawMode drawMode)
@@ -65,11 +65,6 @@ void WebGPURendererAPI::DrawIndexed(uint32_t indexCount, uint32_t indexStart, ui
 	m_RenderPass.drawIndexed(indexCount, 1, indexStart, vertexOffset, 0);
 }
 
-void WebGPURendererAPI::DrawLines(uint32_t vertexCount)
-{
-}
+void WebGPURendererAPI::DrawLines(uint32_t vertexCount) {}
 
-wgpu::RenderPassEncoder WebGPURendererAPI::GetRenderPass()
-{
-	return m_RenderPass;
-}
+wgpu::RenderPassEncoder WebGPURendererAPI::GetRenderPass() { return m_RenderPass; }
