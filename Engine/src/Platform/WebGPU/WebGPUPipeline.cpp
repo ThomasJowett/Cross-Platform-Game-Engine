cf94720547dc44a5b16c6019a79d7ff1218daf70
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2026-05-17 17:53:09 +0100

    call invalidate from the constructor of WebGPU pipeline

--- Engine/src/Platform/WebGPU/WebGPUPipeline.cpp
+++ Engine/src/Platform/WebGPU/WebGPUPipeline.cpp
@@ -38,24 +38,7 @@ static wgpu::VertexFormat ShaderDataTypeToWebGPU(ShaderDataType type)
 	return wgpu::VertexFormat::Undefined;
 }
 
-WebGPUPipeline::WebGPUPipeline(const Spec& spec)
-{
-	m_TransparencyEnabled = spec.transparencyEnabled;
-	m_BackfaceCull = spec.backFaceCulling;
-	m_Specification = spec;
-
-	wgpu::RenderPipelineDescriptor pipelineDesc;
-
-	wgpu::VertexBufferLayout vertexBufferLayout;
-	// TODO: configure the layout from m_Specification
-
-	Ref<GraphicsContext> context = Application::GetWindow()->GetContext();
-	m_WebGPUContext = std::dynamic_pointer_cast<WebGPUContext>(context);
-	auto device = m_WebGPUContext->GetWebGPUDevice();
-
-
-	m_Pipeline = device.createRenderPipeline(pipelineDesc);
-}
+WebGPUPipeline::WebGPUPipeline(const Spec& spec) { Invalidate(); }
 
 WebGPUPipeline::~WebGPUPipeline()
 {
