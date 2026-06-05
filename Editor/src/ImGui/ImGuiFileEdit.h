0dc93fdb9272f20484f198e49ff38ea0b1daecce
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2026-06-05 16:51:16 +0100

    Update imgui file edit to use the new filter lists

--- Editor/src/ImGui/ImGuiFileEdit.h
+++ Editor/src/ImGui/ImGuiFileEdit.h
@@ -3,10 +3,11 @@
 #include <filesystem>
 
 #include "Viewers/ViewerManager.h"
+#include "FileSystem/FileDialog.h"
 
 namespace ImGui
 {
-	bool FileEdit(const char* label, std::filesystem::path& filepath, const wchar_t* filter);
+	bool FileEdit(const char* label, std::filesystem::path& filepath, const std::vector<DialogFilterItem>& filters);
 
 	bool FileEdit(const char* label, std::filesystem::path& filepath, ::FileType filetype);
 
