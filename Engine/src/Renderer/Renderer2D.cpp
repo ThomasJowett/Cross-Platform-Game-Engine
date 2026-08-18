63562a16f284cc479e4f3b424da538d44e658684
Thomas Jowett <thomas_jowett@hotmail.co.uk>
2026-08-18 21:59:39 +0100

    enable depth test on hairlines

--- Engine/src/Renderer/Renderer2D.cpp
+++ Engine/src/Renderer/Renderer2D.cpp
@@ -322,7 +322,6 @@ bool Renderer2D::Init()
 	hairLineSpec.layout = s_Data.hairLineVertexBuffer->GetLayout();
 	hairLineSpec.targetFormats = { FrameBufferTextureFormat::RGBA8, FrameBufferTextureFormat::RED_INTEGER };
 	hairLineSpec.topology = PrimitiveTopology::Lines;
-	hairLineSpec.depthTest = false;
 	s_Data.hairLinePipeline = Pipeline::Create(hairLineSpec);
 
 	Pipeline::Spec textSpec;
